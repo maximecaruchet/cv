e48a2b2993a7236c4d2df258b7b3314b7262f85b
Maxime Caruchet <caruchet.maxime@gmail.com>
2017-05-12 23:04:16 +0200

    V2 - Add smooth scrolling

--- js/init.js
+++ js/init.js
@@ -4,5 +4,17 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+    var $root = $('html, body');
+    $('a').click(function() {
+      if ($.attr(this, 'href')[0] === '#') {
+        $root.animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+        return false;
+      } else {
+        return true;
+      }
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
